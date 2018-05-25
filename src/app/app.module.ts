440228bea1ac0845f5737c22c9f8e7e5a51c9ac6
SvenA <Sven.Anker@ortec-finance.com>
2018-05-25 12:46:28 +0200

    Fixed styling of graph cards and made a definition of material modules in the app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-
-import {AppComponent} from './app.component';
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {environment} from '../environments/environment';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {LayoutModule} from '@angular/cdk/layout';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
 import {
     MatButtonModule,
     MatCardModule,
@@ -17,21 +16,34 @@ import {
     MatSidenavModule,
     MatToolbarModule
 } from '@angular/material';
-import {FeedComponent} from './feed/feed.component';
-import {CardDirective} from './feed/card/card.directive';
-import {ComplexesAnalyzedComponent} from './feed/card/assetmanagementcards/complexesanalyzedcard/complexes-analyzed.component';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {WhatsNewComponent} from './feed/card/assetmanagementcards/whatsnew/whats-new.component';
-import {ProgressCardComponent} from './feed/card/assetmanagementcards/progress-card/progress-card.component';
-import {Ng2GoogleChartsModule} from 'ng2-google-charts';
-import {PieChartComponent} from './feed/card/assetmanagementcards/graphcards/piecharts/piechart.component';
-import {ChartsModule} from 'ng2-charts';
-import {LineChartComponent} from './feed/card/assetmanagementcards/graphcards/linechart/linechart.component';
-import {BarChartComponent} from './feed/card/assetmanagementcards/graphcards/barchart/barchart.component';
-import {DoughnutChartComponent} from './feed/card/assetmanagementcards/graphcards/doughnutchart/doughnutchart.component';
-import {RadarChartComponent} from './feed/card/assetmanagementcards/graphcards/radarchart/radarchart.component';
-import {PolarChartComponent} from './feed/card/assetmanagementcards/graphcards/polarchart/polarchart.component';
-import {ReminderComponent} from './feed/card/assetmanagementcards/reminder/reminder.component';
+import { FeedComponent } from './feed/feed.component';
+import { CardDirective } from './feed/card/card.directive';
+import { ComplexesAnalyzedComponent } from './feed/card/assetmanagementcards/complexesanalyzedcard/complexes-analyzed.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { WhatsNewComponent } from './feed/card/assetmanagementcards/whatsnew/whats-new.component';
+import { ProgressCardComponent } from './feed/card/assetmanagementcards/progress-card/progress-card.component';
+import { Ng2GoogleChartsModule } from 'ng2-google-charts';
+import { PieChartComponent } from './feed/card/assetmanagementcards/graphcards/piecharts/piechart.component';
+import { ChartsModule } from 'ng2-charts';
+import { LineChartComponent } from './feed/card/assetmanagementcards/graphcards/linechart/linechart.component';
+import { BarChartComponent } from './feed/card/assetmanagementcards/graphcards/barchart/barchart.component';
+import { DoughnutChartComponent } from './feed/card/assetmanagementcards/graphcards/doughnutchart/doughnutchart.component';
+import { RadarChartComponent } from './feed/card/assetmanagementcards/graphcards/radarchart/radarchart.component';
+import { PolarChartComponent } from './feed/card/assetmanagementcards/graphcards/polarchart/polarchart.component';
+import { NewsCardComponent } from './feed/card/assetmanagementcards/news-card/news-card.component';
+import { ReminderComponent } from './feed/card/assetmanagementcards/reminder/reminder.component';
+
+const MATERIAL_MODULES = [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatProgressBarModule
+];
 
 @NgModule({
     declarations: [
@@ -40,14 +52,15 @@ import {ReminderComponent} from './feed/card/assetmanagementcards/reminder/remin
         CardDirective,
         ComplexesAnalyzedComponent,
         WhatsNewComponent,
-        ProgressCardComponent,
         PieChartComponent,
         LineChartComponent,
         BarChartComponent,
         DoughnutChartComponent,
         RadarChartComponent,
         PolarChartComponent,
+        ProgressCardComponent,
         ReminderComponent,
+        NewsCardComponent,
     ],
     entryComponents: [
         ComplexesAnalyzedComponent,
@@ -59,22 +72,16 @@ import {ReminderComponent} from './feed/card/assetmanagementcards/reminder/remin
         DoughnutChartComponent,
         RadarChartComponent,
         PolarChartComponent,
-        ReminderComponent
+        PolarChartComponent,
+        ReminderComponent,
+        NewsCardComponent,
     ],
     imports: [
         BrowserModule,
         ServiceWorkerModule.register(environment.serviceWorkerLocation, {enabled: environment.production}),
         BrowserAnimationsModule,
         LayoutModule,
-        MatToolbarModule,
-        MatButtonModule,
-        MatSidenavModule,
-        MatIconModule,
-        MatListModule,
-        MatGridListModule,
-        MatCardModule,
-        MatMenuModule,
-        MatProgressBarModule,
+        MATERIAL_MODULES,
         FlexLayoutModule,
         Ng2GoogleChartsModule,
         ChartsModule
