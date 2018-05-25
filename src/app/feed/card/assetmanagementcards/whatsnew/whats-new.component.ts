88a6ea5f02c4ef40fef13cfaeed42b07c86be942
SvenA <Sven.Anker@ortec-finance.com>
2018-05-25 16:24:52 +0200

    Made app more responsive (fixed youtube video and article with image , on mobile)

--- src/app/feed/card/assetmanagementcards/whatsnew/whats-new.component.ts
+++ src/app/feed/card/assetmanagementcards/whatsnew/whats-new.component.ts
@@ -3,6 +3,7 @@ import {CardComponent} from '../../CardComponent';
 
 @Component({
     templateUrl: './whats-new.component.html',
+    styleUrls: ['./whats-new.component.scss'],
 })
 export class WhatsNewComponent implements CardComponent {
     @Input() data: {
