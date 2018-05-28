353369df55b2bd2692f2a4da22ae3b6cd5bffc03
jelleverstegen <jelle.verstegen@gmail.com>
2018-05-28 10:36:07 +0200

    add image to assets

--- src/app/AppModelFactory.ts
+++ src/app/AppModelFactory.ts
@@ -54,7 +54,7 @@ export class AppModelFactory {
                 [new NewsData({
                         title: 'Interactieve conjunctuurgrafiek', date: '2018-05-17T09:03:01+0200',
                         teaser: 'Conjunctuurcycli zijn een van de belangrijkste kenmerken (‘stylized facts’) die aangeven hoe assetprijzen, rentes, volatiliteiten en macro-economische variabelen zich in de realiteit gedragen.',
-                        imageUrl: 'http://insights.ortec-finance.com/-/media/OF8/Insights/business-cycle-outlook-thumb.ashx?mh=450&la=nl-NL&h=321&w=450&mw=450&hash=65E2A11D2101B87B5E917FA1D3A560CBEBE90366'
+                        imageUrl: '/feedprototype/assets/business.jpg'
                     },
                     'http://insights.ortec-finance.com/nl-nl/2018/02/16/interactieve-conjunctuurgrafiek')], [], [], [], []
             ),
