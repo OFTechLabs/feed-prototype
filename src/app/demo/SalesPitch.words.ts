b8226d5c9a29761e3819c564c897f17ffcc1a6b4
jelleverstegen <jelle.verstegen@gmail.com>
2018-05-31 17:00:05 +0200

    understand some more words

--- src/app/demo/SalesPitch.words.ts
+++ src/app/demo/SalesPitch.words.ts
@@ -4,6 +4,8 @@ export const knownWords: LocalizedWords = {
     en: {
         main: [
             'why buy',
+            'why should I buy you',
+            'why should I pay for you',
             'pitch',
             'presentation',
             'sales pitch'
